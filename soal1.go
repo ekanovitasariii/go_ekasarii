c23531e5524760de9d5e13423cbbc1bd355af6e7
eka novita sari <ekatejo1111@gmail.com>
2022-03-04 15:06:19 +0700

    apload

---
+++ soal1.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt")
+
+func main() {
+     // input 
+     t := 20.0
+     r := 4.0
+     p := 3.14
+     l := 2.0 * p * r * (r+t)
+    fmt.Println(l)
+     // kode disini
+ }    
