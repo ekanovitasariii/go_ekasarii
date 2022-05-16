dbc300a2032107b4648f9a4581b4ac5f5f6f5e48
eka novita sari <ekatejo1111@gmail.com>
2022-05-17 01:41:51 +0700

    tugas

--- projec-mini/businesses/savedplace/domain.go
+++ projec-mini/businesses/savedplace/domain.go
@@ -1,1 +1,37 @@
 package savedplace
+
+import (
+	"context"
+	"projecmini/businesses/places"
+	"projecmini/businesses/users"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Domain struct {
+	Id        	uint		`gorm:"primaryKey"`
+	CreatedAt 	time.Time
+	UpdatedAt 	time.Time
+	DeletedAt 	gorm.DeletedAt `gorm:"index"`
+	User_ID		uint
+	User 		users.Domain
+	Place_ID    uint 
+	Place		places.Domain
+}
+
+type SavedPlaceUsecaseInterface interface {
+	Add(ctx context.Context, domain Domain) (Domain, error)
+	GetAll(ctx context.Context) ([]Domain, error)
+	GetByID(id uint, ctx context.Context) (Domain, error)
+	Update(id uint, ctx context.Context, domain Domain) (Domain, error)
+	Delete(id uint, ctx context.Context) ( error)
+}
+
+type SavedPlaceRepoInterface interface { 
+	Add(ctx context.Context, domain Domain) (Domain, error)
+	GetAll(ctx context.Context) ([]Domain, error)
+	GetByID(id uint, ctx context.Context) (Domain, error)
+	Update(id uint, ctx context.Context, domain Domain) (Domain, error)
+	Delete(id uint, ctx context.Context) (error)
+}
---
+++ unsaaved/go.mod
@@ -0,0 +1,3 @@
+module github.com/ekaaa/echo
+
+go 1.17
